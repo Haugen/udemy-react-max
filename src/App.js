8d6a929379dabf826fa093e00164b2081b491a09
Tobias Haugen <hej@tobiashaugen.se>
2018-10-10 18:53:41 -0300

    Initial commit with some first component play.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Person from './Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Tobias', age: 32 },
+      { name: 'Anders', age: 32 },
+      { name: 'Fredrik', age: 30 },
+    ]
+  };
+
+  switchNameHandler = (newName = "Default value?") => {
+    console.log('Heyah!');
+    this.setState({ persons: [
+      { name: newName, age: 32 },
+      { name: newName, age: 32 },
+      { name: newName, age: 200 },
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>A React app!</h1>
+        <button onClick={this.switchNameHandler.bind(this, "Default value works!")}>Change name!</button>
+        <Person 
+          key={this.state.persons.indexOf(this.state.persons[0])}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={() => this.switchNameHandler()}
+        />
+        <Person 
+          key={this.state.persons.indexOf(this.state.persons[1])}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "with new names!")}
+        />
+        <Person 
+          key={this.state.persons.indexOf(this.state.persons[2])}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, "And binding this.")}
+        />
       </div>
     );
   }
---
+++ src/Person.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+let test = [1, 2, 3];
+test = test.map(num => num * 2);
+test = test.filter(num => num < 5);
+test = test.reduce((sum, curr) => sum + curr);
+
+const person = props => {
+  return (
+    <div className="person">
+      <p>A nice paragraph with some array methods working out a number: {test}.</p>
+      <p>My name is {props.name} and I'm {props.age} years old.</p>
+      <button onClick={props.click}>Click me!</button>
+      {props.children}
+    </div>
+  );
+};
+
+export default person;
