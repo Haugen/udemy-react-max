33867871089bc93173427c6642dd8f6595f459b2
Tobias Haugen <hej@tobiashaugen.se>
2018-10-16 15:00:36 -0300

    Minor css modules stuff

--- src/examples/basics/Person.js
+++ src/examples/basics/Person.js
@@ -3,12 +3,16 @@ import React from 'react';
 import styles from './index.module.css';
 
 const person = props => {
+  let buttonClass = styles.Blue;
+
   return (
     <div className={styles.Person}>
       <p>
         My name is {props.name} and I'm {props.age} years old.
       </p>
-      <button onClick={props.deletePerson}>Delete person</button>
+      <button className={buttonClass} onClick={props.deletePerson}>
+        Delete person
+      </button>
       <input onChange={props.changed} value={props.name} />
     </div>
   );
