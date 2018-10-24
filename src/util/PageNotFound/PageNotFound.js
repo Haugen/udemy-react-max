8997b43444f60b24c94dd58bf59e2605d8734852
Tobias Haugen <hej@tobiashaugen.se>
2018-10-24 16:02:10 -0300

    A 404 page that redirects after an amount of seconds.

--- src/util/PageNotFound/PageNotFound.js
+++ src/util/PageNotFound/PageNotFound.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
 import classes from './PageNotFound.module.css';
+import { Redirect } from 'react-router-dom';
 
-const pageNotFound = () => {
-  return(
-    <>
-      <div className={classes.PageNotFound}>
-        <h1>Page not found</h1>
-        <p>Try another URL.</p>
-      </div>
-    </>
-  );
+class pageNotFound extends React.Component {
+  REDIRECT_IN = 10;
+
+  state = {
+    redirect: false,
+    seconds: this.REDIRECT_IN
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ redirect: true });
+    }, this.REDIRECT_IN * 1000);
+
+    this.interval = setInterval(() => {
+      this.setState((state) => { return { seconds: state.seconds - 1 } });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    let redirect = null;
+    let seconds = this.state.seconds;
+
+    if (this.state.redirect) {
+      redirect = <Redirect to='/' />
+    }
+
+    return(
+      <>
+        <div className={classes.PageNotFound}>
+          <h1>Page not found</h1>
+          <p>Try another URL or wait until redirected.</p>
+          {seconds}
+          {redirect}
+        </div>
+      </>
+    );
+  }
 }
 
 export default pageNotFound;
