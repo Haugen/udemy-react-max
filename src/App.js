303ca57bb44a1567afb4daf57d95d700788a1c89
Tobias Haugen <hej@tobiashaugen.se>
2018-11-01 11:40:26 -0300

    Redux assignment

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Posts from './components/Posts/Posts';
-import PostFull from './components/Posts/Post/PostFull';
-import PageNotFound from './util/PageNotFound/PageNotFound';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+
+import Persons from './containers/Persons';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Layout>
-          <>
-            <Switch>
-              <Route path="/posts/:id" exact component={PostFull} />
-              <Route path="/posts" exact component={Posts} />
-              <Route path="/orders" exact component={Orders} />
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/" exact component={BurgerBuilder} />
-              <Route component={PageNotFound} />
-            </Switch>
-          </>
-        </Layout>
-      </Router>
+      <div className="App">
+        <ol>
+          <li>
+            Turn this app into one which does NOT use local state (in
+            components) but instead uses Redux
+          </li>
+        </ol>
+        <Persons />
+      </div>
     );
   }
 }
--- src/util/Axios/Axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const jsonPlaceholder = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
-});
-
-export const firebase = axios.create({
-  baseURL: 'https://udemy-react-1337.firebaseio.com/'
-});
